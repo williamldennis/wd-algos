fbab92946781de86ad7d7f9c8ab4eca073a43a75
Will Dennis <williamldennis@gmail.com>
2025-06-23 17:37:30 -0400

    added buttons

--- src/app/sort/page.tsx
+++ src/app/sort/page.tsx
@@ -15,19 +15,31 @@ export default function Home() {
         splitSort([1, 14, 3, 9, 4])
     })
 
+    const [sortNumbers, setSortNumbers] = useState<number[]>([])
+    const generateNumbers = () => {
+        const newNumbers: number[] = Array.from({ length: 8 }, () => Math.floor(Math.random() * 100))
+        setSortNumbers(newNumbers)
+    }
 
 
 
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-blue-200">
             <main className="flex flex-col gap-[32px] row-start-2 items-start">
-                <h1 className="text-lg font-bold text-blue-950">Sort</h1>
-                <div className=" flex items-center">
-                    <motion.div>
-                        <input className="border-blue-300 border-2 rounded-full text-lg mr-2 p-3 pl-6 h-15 bg-white"></input>
+                <h1 className="text-lg font-bold text-blue-950"> Merge Sort</h1>
+                <div className="flex flex-col items-center">
+                    <motion.div className="p-4 font-bold bg-white rounded-full mb-4 w-full">
+                        {sortNumbers.join(", ")}
                     </motion.div>
                     <motion.div animate={{ rotate: 360 }} transition={{ duration: 2 }}>
-                        <Button className="p-7 text-md h-15 bg-blue-800 rounded-full border-1 border-blue-900">Sort</Button>
+                        <Button
+                            className="p-7 text-md h-15 bg-blue-800 rounded-full border-1 border-blue-900"
+                            onClick={() => generateNumbers()}
+                        >Generate Numbers</Button>
+                        <Button
+                            className="p-7 text-md h-15 bg-green-800 rounded-full border-1 border-green-900 ml-4"
+                            onClick={() => splitSort(sortNumbers)}
+                        >Sort Numbers</Button>
                     </motion.div>
                 </div>
 
