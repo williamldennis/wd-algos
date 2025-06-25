868d21a78de116cc6151cde96dac8b2300b45265
Will Dennis <williamldennis@gmail.com>
2025-06-25 14:38:47 -0400

    added brutalist

--- src/algos/bubblesort.tsx
+++ src/algos/bubblesort.tsx
@@ -1,28 +1,101 @@
 
+///create an array of frames -- each frame is a State of the array
 
-export function bubbleSort(array: number[]): number[] {
+type animationState = {
+    originalArr?: number[],
+    sortedArr?: number[],
+    stepArr?: number[]
 
-    const unsortedArray = array.slice()
-
-    //for every element in the array, swap it with the element to the right [index+1] if it's greater than that element
-    let swapped
-
-    for (let i = 0; i < unsortedArray.length; i++) {
-        if (unsortedArray[i] > unsortedArray[i + 1]) {
-            [unsortedArray[i], unsortedArray[i + 1]] = [unsortedArray[i + 1], unsortedArray[i]]
-            swapped = true
-        } else {
-            return unsortedArray
-            swapped = false
-        }
-    }
-
-
-
-    if (swapped === false) {
-        const sortedArray = unsortedArray
-        return sortedArray
-    }
 }
 
-bubbleSort([3, 5, 2, 10, 4, 8])
+const frames: animationState[] = []
+
+export function bubbleSort(arr: number[]): number[] {
+
+    frames.push({
+        originalArr: arr.slice(),
+    })
+    let swapped: boolean
+
+    do {
+
+        swapped = false
+
+        for (let i = 0; i < arr.length - 1; i++) {
+
+            if (arr[i] > arr[i + 1]) {
+                [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]]
+                frames.push({
+                    stepArr: arr.slice(),
+                })
+                swapped = true
+            }
+        }
+
+
+    } while (swapped == true)
+
+    frames.push({
+        sortedArr: arr.slice(),
+    })
+    console.log("sorted:", arr)
+    console.log("frames:", frames)
+    return arr
+}
+
+bubbleSort([5, 7, 3, 4, 9, 2, 6])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export function bubbleSort(array: number[]): number[] {
+//     console.log("array to sort:", array)
+//     const unsortedArray = array.slice()
+
+//     //for every element in the array, swap it with the element to the right [index+1] if it's greater than that element
+//     let swapped: boolean
+
+//     do {
+//         swapped = false
+
+//         for (let i = 0; i < unsortedArray.length; i++) {
+//             if (unsortedArray[i] > unsortedArray[i + 1]) {
+//                 [unsortedArray[i], unsortedArray[i + 1]] = [unsortedArray[i + 1], unsortedArray[i]]
+//                 swapped = true
+//             }
+//         }
+
+//     } while (swapped == true)
+
+//     console.log("sorted:", unsortedArray)
+//     return unsortedArray
+
+
+// }
+
+// bubbleSort([3, 5, 2, 10, 4, 8])
---
+++ src/app/bubble-sort/page.tsx
@@ -0,0 +1,23 @@
+'use client'
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+export default function BubbleSort() {
+
+
+    return (
+        <div>
+            <h1>Bubble Sort</h1>
+
+        </div>
+    )
+
+}
